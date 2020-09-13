7a2bf9e5bd79d76ea49b00b50697c53fb6aed097
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-14 01:05:04 +0200

    doc(IndexNotFoundException): add documentation for IndexNotFoundException

--- src/main/kotlin/fr/humanapp/meilisearch/exception/IndexNotFoundException.kt
+++ src/main/kotlin/fr/humanapp/meilisearch/exception/IndexNotFoundException.kt
@@ -1,5 +1,8 @@
 package fr.humanapp.meilisearch.exception
 
+/**
+ * Conveys the message that an index does not exist when an attempt to retrieve it is made
+ */
 class IndexNotFoundException : MeilisearchException {
 	constructor(indexName: String) : super("Index \"$indexName\" not found")
 
