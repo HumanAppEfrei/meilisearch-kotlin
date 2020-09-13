ce2266b93f406701591965a4d7d7534e6f2ed5bc
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-13 17:50:49 +0200

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "fr.humanapp"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "meilisearch-kotlin"
+
