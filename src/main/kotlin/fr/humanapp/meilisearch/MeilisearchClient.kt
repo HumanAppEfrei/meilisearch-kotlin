c00620b6b0fab8d12276f03be8373b675da118b3
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-14 00:18:11 +0200

    test(MeilisearchClient): add first tests for MeilisearchClient class

--- src/main/kotlin/fr/humanapp/meilisearch/MeilisearchClient.kt
+++ src/main/kotlin/fr/humanapp/meilisearch/MeilisearchClient.kt
@@ -1,5 +1,6 @@
 package fr.humanapp.meilisearch
 
+
 /**
  * Wrapper class to interact with a Meilisearch database instance
  */
@@ -7,6 +8,19 @@ public class MeilisearchClient(private val config: MeilisearchConfig) {
 	constructor(host: String) : this(MeilisearchConfig(host))
 
 	 init {
-		 TODO("Test connection to Meilisearch host")
+		 // TODO("Test connection to Meilisearch host")
 	 }
+
+	fun createIndex(name: String, primaryKey: String = "id") {
+		TODO("Method not implemented")
+	}
+
+	@Throws(Exception::class)
+	fun getIndex(s: String): Any {
+		TODO("Method not implemented")
+	}
+
+	fun index(name: String): Any? {
+		TODO("Method not implemented")
+	}
 }
--- src/test/kotlin/fr/humanapp/meilisearch/MeilisearchClientTest.kt
+++ src/test/kotlin/fr/humanapp/meilisearch/MeilisearchClientTest.kt
@@ -1,5 +1,48 @@
 package fr.humanapp.meilisearch
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
-internal class MeilisearchClientTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+internal class MeilisearchClientTest {
+	private val msc = MeilisearchClient("http://localhost:7700")
+
+	@Test
+	@Order(1)
+	fun `createIndex() Can create index without custom primary key`() {
+		msc.createIndex("__test_index_1")
+	}
+
+	@Test
+	@Order(2)
+	fun `createIndex() Can create index with custom primary key`() {
+		msc.createIndex("__test_index_2", "custom_id")
+	}
+
+	@Test
+	@Order(3)
+	fun `getIndex() Can retrieve existing index`() {
+		assertNotNull(msc.getIndex("__test_index_1"))
+	}
+
+	@Test
+	@Order(4)
+	fun `getIndex() Cannot retrieve non-existing index`() {
+		assertThrows(Exception::class.java) {
+			msc.getIndex("__no_existing_index")
+		}
+	}
+
+	@Test
+	@Order(5)
+	fun `index() Can dynamically get or create index`() {
+		// With existing index
+		assertNotNull(msc.index("__test_index_1"))
+
+		// With non-existing index
+		assertNotNull(msc.index("__test_index_3"))
+	}
+}
