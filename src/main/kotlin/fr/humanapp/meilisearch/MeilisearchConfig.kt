4f899e094d2ea3482c3b08355d6e8fe0dfdf04f1
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-15 21:55:44 +0200

    feat(MeilisearchConfig): improve host formatting

--- src/main/kotlin/fr/humanapp/meilisearch/MeilisearchConfig.kt
+++ src/main/kotlin/fr/humanapp/meilisearch/MeilisearchConfig.kt
@@ -1,12 +1,20 @@
 package fr.humanapp.meilisearch
 
+import fr.humanapp.meilisearch.exception.MeilisearchException
+
+
 /**
  * Wrapper class representing the configuration for a MeilisearchClient instance
  */
-public data class MeilisearchConfig(val host: String) {
-	private var apiKey: String? = null
+public class MeilisearchConfig @JvmOverloads constructor(host: String, private val apiKey: String? = null) {
+	var host: String private set
 
-	constructor(host: String, apiKey: String) : this(host) {
-		this.apiKey = apiKey
+	// TODO: improve httpRegex
+	private val httpRegex = Regex("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}(\\.[a-zA-Z0-9()]{1,6})?\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)")
+	private fun removeTrailingSlash(host: String) = Regex("/*$").replace(host, "")
+
+	init {
+		if (!httpRegex.matches(host)) throw MeilisearchException("Invalid host \"$host\"")
+		this.host = removeTrailingSlash(host)
 	}
 }
