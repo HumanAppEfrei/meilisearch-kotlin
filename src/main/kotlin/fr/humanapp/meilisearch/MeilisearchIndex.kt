d6496c494b033e6b00e5a4d3ca9030eab473d32d
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-15 23:25:46 +0200

    feat(MeilisearchIndex): make MeilisearchIndex extend IndexDto

--- src/main/kotlin/fr/humanapp/meilisearch/MeilisearchIndex.kt
+++ src/main/kotlin/fr/humanapp/meilisearch/MeilisearchIndex.kt
@@ -1,4 +1,13 @@
 package fr.humanapp.meilisearch
 
-public class MeilisearchIndex {
+import fr.humanapp.meilisearch.dto.IndexDto
+import java.util.*
+
+public class MeilisearchIndex(
+	uid: String,
+	primaryKey: String = "id",
+	name: String = uid,
+	createdAt: Date? = null,
+	updatedAt: Date? = null) : IndexDto(uid, primaryKey, name) {
+
 }
