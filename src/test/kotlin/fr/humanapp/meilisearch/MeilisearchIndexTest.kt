0cb8faa26f6316031760c7864f1309c8fd6509e6
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-16 00:38:23 +0200

    refactor(MeilisearchIndexTest): add companion object to class for static behaviour

--- src/test/kotlin/fr/humanapp/meilisearch/MeilisearchIndexTest.kt
+++ src/test/kotlin/fr/humanapp/meilisearch/MeilisearchIndexTest.kt
@@ -1,16 +1,19 @@
 package fr.humanapp.meilisearch
 
-import org.junit.AfterClass
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class MeilisearchIndexTest {
-	private val name = "__test_index"
-	private val msc = MeilisearchClient("http://localhost:7700")
-	private val index = msc.createIndex(name)
+	private companion object {
+		const val uid = "__test_index"
+		val msc = MeilisearchClient("http://localhost:7700")
+		val index = msc.createIndex(uid)
+
+		@AfterAll @JvmStatic
+		fun `Delete index`() {
+			msc.deleteIndex(uid)
+		}
+	}
 
 	@Test
 	@Order(1)
@@ -22,11 +25,5 @@ class MeilisearchIndexTest {
 	@Order(2)
 	fun `Deleting documents should work`() {
 		TODO("Documents deletion test not implemented")
-	}
-
-
-	@AfterClass
-	fun `Delete index`() {
-		TODO("Index deletion test not implemented")
 	}
 }
