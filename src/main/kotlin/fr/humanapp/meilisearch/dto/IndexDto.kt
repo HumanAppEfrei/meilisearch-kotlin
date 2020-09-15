2bd7fd935eea0d3648764c64fa1875ccbe013ef5
AmiralBl3ndic <camille.briand@efrei.net>
2020-09-15 23:08:44 +0200

    doc(IndexDto): add IndexDto documentation comments

--- src/main/kotlin/fr/humanapp/meilisearch/dto/IndexDto.kt
+++ src/main/kotlin/fr/humanapp/meilisearch/dto/IndexDto.kt
@@ -2,9 +2,29 @@ package fr.humanapp.meilisearch.dto
 
 import java.util.*
 
-data class IndexDto(
+open class IndexDto(
+	/**
+	 * Unique identifier of the index
+	 */
 	val uid: String,
+
+	/**
+	 * Primary key field for documents in that index
+	 */
 	val primaryKey: String = "id",
+
+	/**
+	 * Name of the index (often identical to **uid**)
+	 */
+	val name: String = "",
+
+	/**
+	 * Creation date
+	 */
 	val createdAt: Date? = null,
+
+	/**
+	 * Last update date
+	 */
 	val updatedAt: Date? = null
 )
